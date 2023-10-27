597ebec1a975223aab79d2de6298c2a65a805a2a
Sanusasaas <117139601+Sanusasaas@users.noreply.github.com>
2023-10-27 14:52:11 +0300

    Add files via upload

---
+++ scss/script/script.js
@@ -0,0 +1,36 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
+
+const anchors = document.querySelectorAll('a.scroll-to')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href')
+    
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+const burger = document.querySelector('.burger');
+const header = document.querySelector('header');
+
+window.addEventListener("DOMContentLoaded", function() {
+  burger.addEventListener("click", function() {
+    header.classList.toggle("open")
+  })
+})
